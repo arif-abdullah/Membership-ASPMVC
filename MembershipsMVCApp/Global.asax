21be7907ef1e217333a9944a4557793b995de268
arif-abdullah <arifabdullah.pfi@gmail.com>
2018-01-21 12:53:44 +0600

    First Commit

---
+++ MembershipsMVCApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MembershipsMVCApp.MvcApplication" Language="C#" %>
---
+++ MembershipsMVCApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MembershipsMVCApp.Startup))]
+namespace MembershipsMVCApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
